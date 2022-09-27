f2ee99e9a92a6f89e2a83b969d4f25cc39cc954c
Victor Cesar Ferreira <99146095+VictorDron@users.noreply.github.com>
2022-09-27 19:09:56 -0300

    uP

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
+function calcRSI(closes) {
+
+    let gains = 0;
+    let losses = 0;
+    
+    for(let i = closes.length - 6; i < closes.length; i++){
+        const diff = closes[i] - closes [i - 1];
+        if (diff >= 0)
+           gains += diff;
+        else
+           losses -= diff;   
+    }
+    const strength = gains/losses;
+    return 100 - (100/ (1 + strength));
+    
+}
 
 function cot () {
     
@@ -5,23 +21,6 @@ function cot () {
     ajax.open('GET', 'https://api.binance.com/api/v3/klines?symbol=ETHUSDT&interval=1m');
     ajax.send();
 
-
-    function calcRSI(closes) {
-
-        let gains = 0;
-        let losses = 0;
-        
-        for(let i = obj.length - 6; i < obj.length; i++){
-            const diff = obj[i] -  obj[i - 1];
-            if (diff >= 0)
-               gains += diff;
-            else
-               losses -= diff;   
-        }
-        const strength = gains/losses;
-        return 100 - (100/ (1 + strength));
-        
-    }
     
     ajax.onload = function () {
         
@@ -31,16 +30,13 @@ function cot () {
         console.log(obj.length);
         document.getElementById('texto2').innerHTML = price0;
         
-        const closes = obj.data.map(candle => parseFloat(candle[4]))
+        const closes = closes.data.map(candle => parseFloat(candle[4]))
         let rsi = parseFloat(calcRSI(closes));
         console.log(rsi);
         
 
     }
-
-    
     
 }
     
-
 setInterval(cot,1000);
